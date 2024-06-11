45fdcd7a5ea15a389a514ba8e154e0336eb5aca5
sproshchaev <sproshchaev@gmail.com>
2024-06-11 23:50:34 +0500

    feat: add starter-amqp, spring-kafka

--- kafka/producer-service/build.gradle.kts
+++ kafka/producer-service/build.gradle.kts
@@ -7,13 +7,21 @@ plugins {
 group = "com.prosoft"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.kafka:spring-kafka")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- rabbitmq/build.gradle.kts
+++ rabbitmq/build.gradle.kts
@@ -5,13 +5,21 @@ plugins {
 group = "com.prosoft"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-amqp")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.amqp:spring-rabbit-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
